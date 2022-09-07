aacbef3c159b7e8823e9b8c934efe8688d863b69
hp <davidebula642@gmail.com>
2022-09-07 10:18:08 +0200

    programing is fun

---
+++ src/App.js
@@ -0,0 +1,16 @@
+ 
+import About from './Components/About';
+import Home from './Components/Home';
+import NavBar from './Components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Home/>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
