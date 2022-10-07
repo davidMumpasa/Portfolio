3d3d7cdaad57c05d752152e4ca58d75087a74184
hp <davidebula642@gmail.com>
2022-10-07 20:56:52 +0200

    a few updates

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import About from "./Components/About";
-import Contact from "./Components/Contact";
-import Home from "./Components/Home";
-import NavBar from "./Components/NavBar";
-import Skills from "./Components/Skills";
-import Work from "./Components/Work";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Home from './Components/Home';
+import NavBar from './Components/NavBar';
+import Skills from './Components/Skills';
+import Work from './Components/Work';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/privacy-policy" element={()=>{
-            window.location.href = 'www.linkedin.com/in/david-ebula-10b1b2231'; 
-            return null;
-          }} />
-        </Routes>
-      </BrowserRouter>
-
-      <NavBar />
-      <Home />
-      <About />
-      <Skills />
-      <Work />
-      <Contact />
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Skills/>
+       <Work/>
+      <Contact/>
     </div>
   );
 }
