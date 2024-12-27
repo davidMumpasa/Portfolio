05c3c49ba288485bb7a4f282ab38b610e60881e8
DavidMumpasa <davidebula642@gmail.com>
2024-12-27 20:00:20 +0200

    Version 2

--- src/Components/About.js
+++ src/Components/About.js
@@ -19,11 +19,31 @@ const About = () => {
 
           <div>
             <p>
-              I am an AI Engineer, Full-Stack Software Developer, and DevOps Engineer with a strong passion for solving complex problems and delivering innovative, high-quality solutions. Over the past two years, I have gained extensive experience in developing web-based and mobile applications, working on both the front-end and back-end using technologies such as React.js, Angular.js, React Native, Flask, Spring Boot, Java EE, Android Studio, and RESTful APIs.
-              <br /><br />
-              In addition to my software development expertise, I have a deep understanding of AI technologies, enabling me to build intelligent systems that leverage machine learning and data-driven insights. As a dedicated tester, I have worked with automation testing tools such as Selenium and Appium to ensure the quality and reliability of software.
-              <br /><br />
-              As a DevOps Engineer, I have worked with Jenkins to streamline and automate development pipelines, improving efficiency and scalability. With a strong foundation in both development and testing, I excel at creating efficient, scalable solutions that meet real-world needs, whether it's building intuitive user interfaces, robust back-end systems, or automating processes for continuous integration and deployment.
+              I am an AI Engineer, Full-Stack Software Developer, and DevOps
+              Engineer with a strong passion for solving complex problems and
+              delivering innovative, high-quality solutions. Over the past two
+              years, I have gained extensive experience in developing web-based
+              and mobile applications, working on both the front-end and
+              back-end using technologies such as React.js, Angular.js, React
+              Native, Flask, Spring Boot, Java EE, Android Studio, and RESTful
+              APIs.
+              <br />
+              <br />
+              In addition to my software development expertise, I have a deep
+              understanding of AI technologies, enabling me to build intelligent
+              systems that leverage machine learning and data-driven insights.
+              As a dedicated tester, I have worked with automation testing tools
+              such as Selenium and Appium to ensure the quality and reliability
+              of software.
+              <br />
+              <br />
+              As a DevOps Engineer, I have worked with Jenkins to streamline and
+              automate development pipelines, improving efficiency and
+              scalability. With a strong foundation in both development and
+              testing, I excel at creating efficient, scalable solutions that
+              meet real-world needs, whether it's building intuitive user
+              interfaces, robust back-end systems, or automating processes for
+              continuous integration and deployment.
             </p>
           </div>
         </div>
